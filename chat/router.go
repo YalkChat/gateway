79608aa37906be5397cc6f29b1aa9912629b4c76
Revengeic3 <onair95@gmail.com>
2023-05-08 17:31:59 +0200

    Separate encoding and decoding logic

--- chat/router.go
+++ chat/router.go
@@ -1,44 +1,39 @@
 package chat
 
 import (
-	"encoding/json"
 	"yalk-backend/logger"
 )
-
-func encodePayload(event *EventMessage) ([]byte, error) {
-	eventMessageJson, err := json.Marshal(event)
-	if err != nil {
-		return nil, err
-	}
-	return eventMessageJson, nil
-}
 
 // Echoing to client is default behavior for error checking.
 
 // Broadcast to all
 // TODO: Error checking
 func (server *Server) SendMessage(event *EventMessage) {
+	payload, err := encodeEventMessage(event)
+
+	if err != nil {
+		logger.Err("ROUTER", "Error encoding payload")
+	}
+
 	for userId, client := range server.Clients {
 		if userId != event.Sender {
-			payload, err := encodePayload(event)
-			if err != nil {
-				logger.Err("ROUTER", "Error encoding payload")
-			}
 			client.Msgs <- payload
 		}
 	}
 }
 
 // Send to one or multiple connected clients
-func (server *Server) SendToClients(event *EventMessage) {
+func (server *Server) SendMessageToAll(event *EventMessage) {
+	payload, err := encodeEventMessage(event)
+
+	if err != nil {
+		logger.Err("ROUTER", "Error encoding payload")
+	}
+
 	for _, id := range event.Receivers {
 		wsClient := server.Clients[id]
 		if wsClient != nil {
-			payload, err := encodePayload(event)
 
-			if err != nil {
-				logger.Err("ROUTER", "Error encoding payload")
-			}
 			wsClient.Msgs <- payload
 		}
 	}
@@ -64,7 +59,7 @@ func (server *Server) Router() {
 
 		case event := <-server.Channels.Dm:
 			logger.Info("ROUTER", "Router: Dm received")
-			server.SendToClients(event)
+			server.SendMessageToAll(event)
 
 		}
 	}
