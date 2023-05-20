8218d1a7ed3427ca4d4f4d3b7e15b6c8c9bc7090
Revengeic3 <onair95@gmail.com>
2023-05-20 08:39:40 +0200

    Rename sessions.go to main.go

---
+++ sessions/main.go
@@ -0,0 +1,61 @@
+package sessions
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"gorm.io/gorm"
+)
+
+type Credentials struct {
+	ID       int    `json:"id"`
+	Email    string `json:"email,omitempty"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	jwt.StandardClaims
+}
+
+type Token = string
+type UserID = int
+type SessionLenght = time.Time
+
+func New(db *gorm.DB, dl time.Duration) *Manager {
+	db.AutoMigrate(&Credentials{}, &Session{})
+	return &Manager{
+		defaultLenght:  dl,
+		activeSessions: make([]*Session, 0),
+	}
+}
+
+type Session struct {
+	ID      uint
+	UserID  UserID
+	Token   string `gorm:"primaryKey"`
+	Created time.Time
+	Expiry  time.Time
+}
+
+func (s *Session) SetClientCookie(w http.ResponseWriter) {
+	http.SetCookie(w, &http.Cookie{
+		Name:    "YLK", // TODO: Put SessionManager name
+		Value:   s.Token,
+		Expires: s.Expiry,
+		// SameSite: http.SameSiteNoneMode,
+		// Secure:   true, //! SET AGAIN WHEN USING HTTPS
+		HttpOnly: true,
+		Path:     "/",
+	})
+}
+
+func (s *Session) isExpired() bool {
+	return s.Expiry.Before(time.Now())
+}
+
+// func store(db *sql.DB, id string, token string, created time.Time, expires time.Time) (err error) {
+
+// 	return
+// }
