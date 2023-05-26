51cc8f8827e9594e01766b993303ce0a41bf409c
Revengeic3 <onair95@gmail.com>
2023-05-26 04:44:12 +0200

    Add methods to Account type

--- chat/accounts.go
+++ chat/accounts.go
@@ -10,7 +10,8 @@ type Account struct {
 	gorm.Model
 	Email    string `json:"email"`
 	Username string `json:"username"`
-	Password string `json:"password"`
+	Password string `json:"password"` // ! NOPE
+	Verified bool   `json:"verified"`
 }
 
 func (account *Account) Type() string {
@@ -25,6 +26,10 @@ func (account *Account) Deserialize(data []byte) error {
 	return json.Unmarshal(data, account)
 }
 
-func (c *Account) Create(db *gorm.DB) error {
-	return db.Create(&c).Error
+func (account *Account) Create(db *gorm.DB) error {
+	return db.Create(&account).Error
+}
+
+func (account *Account) GetInfo(db *gorm.DB) error {
+	return db.Find(&account).Error
 }
