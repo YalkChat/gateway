e110cc7187730a2ff35d22643a2be378b4904e69
AleRosmo <onair95@gmail.com>
2023-08-29 18:46:01 +0200

    Add type assertion

--- newchat/event/handlers/new_message.go
+++ newchat/event/handlers/new_message.go
@@ -4,23 +4,27 @@ import (
 	"log"
 	"yalk/database"
 	"yalk/database/models"
+	"yalk/newchat/event"
+	"yalk/newchat/event/types"
 
 	"gorm.io/gorm"
 )
 
 type NewMessageHandler struct{}
 
-func (e NewMessageHandler) HandleEvent(db *gorm.DB, event NewMessageHandler) error {
-	// Step 1: Validation
+func (h NewMessageHandler) HandleEvent(db *gorm.DB, e event.Event) error {
+
+	// Step 1: Convert e to NewMessageEvent type
+	newMessageEvent := e.(types.NewMessageEvent)
+
 	// TODO: Evaluate if the logic of authentication and validation can be a Higher order function, or plug-n-play, whatever it's called
 	// TODO: Evaluate if event to handle should be in struct properties or as function argument
-	if err := validateMessageCreate(event); err != nil {
+	if err := validateMessageCreate(newMessageEvent); err != nil {
 		log.Printf("Validation failed: %v", err)
 		return err
 	}
 
 	// Step 2: Make Database type
-
 	if err := database.AddMessage(db, &models.Message{}); err != nil {
 		return err
 	}
@@ -30,11 +34,11 @@ func (e NewMessageHandler) HandleEvent(db *gorm.DB, event NewMessageHandler) err
 		return err
 	}
 
-	log.Printf("Handling MessageCreate event: %v", e)
+	log.Printf("Handling MessageCreate event: %v", h)
 	return nil
 }
 
-func validateMessageCreate(eventData NewMessageHandler) error {
+func validateMessageCreate(eventData types.NewMessageEvent) error {
 	// Validate the message content, ChatID, SenderID, etc.
 	// Return an error if validation fails
 	return nil
