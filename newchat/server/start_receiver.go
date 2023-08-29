5dfbf66af89d7adeaf7c120e2e17e74bc6bd292d
AleRosmo <onair95@gmail.com>
2023-08-30 01:59:34 +0200

    Implement message handling

--- newchat/server/start_receiver.go
+++ newchat/server/start_receiver.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"encoding/json"
 	"log"
 	"yalk/newchat/client"
 	"yalk/newchat/event"
+	"yalk/newchat/models"
 )
 
 func (s *serverImpl) StartReceiver(client client.Client, eventChannel chan event.Event, quit chan struct{}) {
@@ -19,10 +21,19 @@ func (s *serverImpl) StartReceiver(client client.Client, eventChannel chan event
 				log.Printf("Error reading event from client %s: %v", client.ID(), err)
 				break
 			}
-			// Forward the event to the server for handling
-			if err := s.HandleEvent(receivedEvent); err != nil {
-				// Handle the error, possibly by logging it
-				log.Printf("Error handling event from client %s: %v", client.ID(), err)
+			// TODO: Missing EOF check but unsure if needed
+			if messageType.String() == "MessageText" {
+				baseEvent := &models.BaseEvent{} // Assuming BaseEvent is your new RawEvent
+				err := json.Unmarshal(receivedEvent, baseEvent)
+				if err != nil {
+					log.Println("Error unmarshalling:", err)
+					continue
+				}
+				// Forward the event to the server for handling
+				if err := s.HandleEvent(baseEvent); err != nil {
+					// Handle the error, possibly by logging it
+					log.Printf("Error handling event from client %s: %v", client.ID(), err)
+				}
 			}
 		}
 	}
