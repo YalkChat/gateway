e394ff9fd7cf24fa7f24792268bf4c521402ad83
AleRosmo <onair95@gmail.com>
2023-08-29 18:59:56 +0200

    Return correct type

--- newchat/event/types/new_message.go
+++ newchat/event/types/new_message.go
@@ -3,14 +3,14 @@ package types
 import "encoding/json"
 
 type NewMessageEvent struct {
-	clientID  string
-	data      json.RawMessage
-	eventType string
+	clientID string
+	data     json.RawMessage
+	// eventType string
 }
 
 // TODO: This must go in the RawPayload
 func (e NewMessageEvent) Type() string {
-	return e.eventType
+	return "NewMessage"
 }
 
 func (e NewMessageEvent) Data() json.RawMessage {
