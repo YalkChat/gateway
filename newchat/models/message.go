fde9afe35bcb0fc1b1f2a93d4063b36e0fcfb171
AleRosmo <onair95@gmail.com>
2023-08-30 03:56:04 +0200

    Add important todo

--- newchat/models/message.go
+++ newchat/models/message.go
@@ -2,6 +2,12 @@ package models
 
 import "time"
 
+// TODO: Very important To-Do: it is possible that this type will need to
+// TODO: become NewMessage, evaluate why it would be needed, the doubt is the
+// TODO: additional informations that the event could need to carry but for now
+// TODO: none came to my mind, and also it looks suspicious that it's almost
+// TODO: identical to it's database models counterpart.
+
 type Message struct {
 	ID        uint      `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
