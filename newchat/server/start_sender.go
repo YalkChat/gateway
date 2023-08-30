84f53411efd7e4b910fefe5252493938a6e4d471
AleRosmo <onair95@gmail.com>
2023-08-30 03:56:29 +0200

    Change code skeleton base

--- newchat/server/start_sender.go
+++ newchat/server/start_sender.go
@@ -2,13 +2,21 @@ package server
 
 import (
 	"yalk/newchat/client"
-	"yalk/newchat/event"
 )
 
-func (s *serverImpl) StartSender(c client.Client, outgoingEvents chan event.Event) {
-	for event := range outgoingEvents {
-		if err := c.SendEvent(event); err != nil {
-			// handle or log the error
+func (s *serverImpl) StartSender(c client.Client, quit chan struct{}) {
+	for {
+		select {
+		case <-quit:
+			// Handle cleanup if needed
+			return
+		default:
 		}
 	}
+
+	// for event := range outgoingEvents {
+	// 	if err := c.SendEvent(event); err != nil {
+	// 		// handle or log the error
+	// 	}
+	// }
 }
