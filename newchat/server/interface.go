eb2b9ffaab517e8d24d4f316c741c372ea9930a4
AleRosmo <onair95@gmail.com>
2023-08-30 03:56:18 +0200

    Introduce BaseEventWithContext

--- newchat/server/interface.go
+++ newchat/server/interface.go
@@ -10,7 +10,7 @@ type Server interface {
 	UnregisterClient(client client.Client) error
 	// TODO: Should I use a client for models.Message?
 	SendToChat(message models.Message) error
-	HandleEvent(event *models.BaseEvent) error
+	HandleEvent(event *models.BaseEventWithMetadata) error
 }
 
 // Additional type definitions for Client, Message, etc.
