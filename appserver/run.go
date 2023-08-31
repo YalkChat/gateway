c9a7495d356833521a9f1799e0fa291428c84f53
AleRosmo <onair95@gmail.com>
2023-09-01 01:18:43 +0200

    Add test initialization of newChatServer

--- appserver/run.go
+++ appserver/run.go
@@ -6,21 +6,23 @@ import (
 	"time"
 	"yalk/chat/server"
 	"yalk/config"
+	"yalk/database"
 	newserver "yalk/newchat/server"
 	"yalk/sessions"
 
 	"gorm.io/gorm"
 )
 
-func RunServer(config *config.Config, db *gorm.DB) {
+func RunServer(config *config.Config, conn *gorm.DB) {
 	var wg sync.WaitGroup
 
 	sessionLenght := time.Hour * 720
-	sessionsManager := sessions.New(db, sessionLenght)
+	sessionsManager := sessions.New(conn, sessionLenght)
 
-	chatServer := server.NewServer(16, db, sessionsManager)
+	chatServer := server.NewServer(16, conn, sessionsManager)
+	db := database.NewDatabase(conn)
 	newChatServer := newserver.NewServer(db)
-	fmt.Print(newChatServer)
+	fmt.Print(newChatServer) // TODO: remove
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
