27f848d7ed01b52cb3c807efa3b655cdfa231782
AleRosmo <onair95@gmail.com>
2023-09-01 01:48:24 +0200

    Add user type

---
+++ database/models/user.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        string    `gorm:"primaryKey"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	Username  string
+}
