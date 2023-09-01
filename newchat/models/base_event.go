156e7e1d88ce77b527ad6f4175a0abd778f4eb6f
AleRosmo <onair95@gmail.com>
2023-09-01 02:33:22 +0200

    Change ClientID type

--- newchat/models/base_event.go
+++ newchat/models/base_event.go
@@ -5,6 +5,6 @@ import "encoding/json"
 type BaseEvent struct {
 	Opcode   string          `gorm:"-" json:"opcode"`
 	Data     json.RawMessage `gorm:"-" json:"data"`
-	ClientID uint            `gorm:"-" json:"clientID"`
+	ClientID string          `gorm:"-" json:"clientID"`
 	Type     string          `gorm:"-" json:"type"`
 }
