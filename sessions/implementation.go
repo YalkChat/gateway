38cec26f2e210030f6fd7a4e446d6f88100c5707
AleRosmo <onair95@gmail.com>
2023-09-05 01:13:39 +0200

    Add sessionDB implementation.

--- sessions/implementation.go
+++ sessions/implementation.go
@@ -1,1 +1,36 @@
 package sessions
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type SessionDB struct{}
+
+func (sdb *SessionDB) SaveSession(db *gorm.DB, token string, id uint, lenght time.Time) (*Session, error) {
+	session := &Session{
+		Token:     token,
+		AccountID: id,
+		ExpiresAt: lenght,
+	}
+	result := db.Create(session)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return session, nil
+}
+
+func (sdb *SessionDB) LoadSession(db *gorm.DB, token string) (*Session, error) {
+	var session Session
+	result := db.Where("token = ?", token).First(&session)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &session, nil
+}
+
+func (sdb *SessionDB) DeleteSession(db *gorm.DB, token token) error {
+	result := db.Where("token = ?", token).Delete(&Session{})
+	return result.Error
+}
