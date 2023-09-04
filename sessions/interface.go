eed32f2c89d5715ea75d292c17e177645291181f
AleRosmo <onair95@gmail.com>
2023-09-05 01:14:28 +0200

    Add SessionManager and update SessionDatabase definitions

--- sessions/interface.go
+++ sessions/interface.go
@@ -1,9 +1,20 @@
 package sessions
 
-import "database/sql"
+import (
+	"net/http"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type SessionDatabase interface {
-	SaveSession(*sql.DB, Token, uint, SessionLenght) *Session
-	LoadSession(*sql.DB, string) error
-	DeleteSession(*sql.DB, string) (*Session, error)
+	SaveSession(*gorm.DB, token, uint, time.Time) (*Session, error)
+	LoadSession(*gorm.DB, token) (*Session, error)
+	DeleteSession(*gorm.DB, string) error
+}
+
+type SessionManager interface {
+	Create(*gorm.DB, token, uint, time.Time) (*Session, error)
+	Validate(*gorm.DB, *http.Request, string) (*Session, error)
+	Delete(*gorm.DB, token) error
 }
