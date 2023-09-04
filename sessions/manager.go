b0601f5e980f09617bb14a771186dc3e89b43e40
AleRosmo <onair95@gmail.com>
2023-09-05 01:27:31 +0200

    Comment out all functions

--- sessions/manager.go
+++ sessions/manager.go
@@ -1,13 +1,9 @@
 package sessions
 
 import (
-	"errors"
-	"log"
-	"net/http"
 	"time"
 
 	"github.com/golang-jwt/jwt"
-	"gorm.io/gorm"
 )
 
 type Claims struct {
@@ -35,77 +31,77 @@ type SessionLenght = time.Time
 
 // TODO: Redis will be used for this at some point in development.
 // TODO: Some decent in error checking would be nice.
-func (sm *sessionManagerImpl) Create(db *gorm.DB, token Token, id uint, lenght SessionLenght) (*Session, error) {
-	var _lenght time.Time = time.Now().Add(sm.defaultLenght)
+// func (sm *sessionManagerImpl) Create(db *gorm.DB, token Token, id uint, lenght SessionLenght) (*Session, error) {
+// 	var _lenght time.Time = time.Now().Add(sm.defaultLenght)
 
-	if !lenght.IsZero() {
-		_lenght = lenght
-	}
+// 	if !lenght.IsZero() {
+// 		_lenght = lenght
+// 	}
 
-	session := &Session{
-		AccountID: id,
-		Token:     token,
-		Created:   time.Now(),
-		Expiry:    _lenght,
-	}
+// 	session := &Session{
+// 		AccountID: id,
+// 		Token:     token,
+// 		Created:   time.Now(),
+// 		Expiry:    _lenght,
+// 	}
 
-	if tx := db.Create(&session); tx.Error != nil {
-		return nil, tx.Error
-	}
+// 	if tx := db.Create(&session); tx.Error != nil {
+// 		return nil, tx.Error
+// 	}
 
-	sm.activeSessions = append(sm.activeSessions, session)
-	log.Println("Saved new session")
-	return session, nil
-}
+// 	sm.activeSessions = append(sm.activeSessions, session)
+// 	log.Println("Saved new session")
+// 	return session, nil
+// }
 
-func (sm *sessionManagerImpl) Validate(db *gorm.DB, r *http.Request, cookieName string) (*Session, error) {
-	// TODO: This requires redis?
-	// TODO: Further cookie properties check
-	// ? If loading from DB data, this is 'redundant'
-	// Server might have restarted, search DB and compare - fuck them they just log in again
+// func (sm *sessionManagerImpl) Validate(db *gorm.DB, r *http.Request, cookieName string) (*Session, error) {
+// 	// TODO: This requires redis?
+// 	// TODO: Further cookie properties check
+// 	// ? If loading from DB data, this is 'redundant'
+// 	// Server might have restarted, search DB and compare - fuck them they just log in again
 
-	cookie, err := r.Cookie("YLK")
-	if err != nil {
-		return nil, errors.New("cookie_missing")
-	}
+// 	cookie, err := r.Cookie("YLK")
+// 	if err != nil {
+// 		return nil, errors.New("cookie_missing")
+// 	}
 
-	err = cookie.Valid()
-	if err != nil {
-		return nil, err
-	}
+// 	err = cookie.Valid()
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	for _, session := range sm.activeSessions {
-		if cookie.Value == session.Token {
-			log.Println("Found stored active session")
-			return session, nil
-		}
-	}
+// 	for _, session := range sm.activeSessions {
+// 		if cookie.Value == session.Token {
+// 			log.Println("Found stored active session")
+// 			return session, nil
+// 		}
+// 	}
 
-	session := &Session{}
+// 	session := &Session{}
 
-	if tx := db.First(&session, "token = ?", cookie.Value); tx.Error != nil {
-		return nil, tx.Error
-	}
+// 	if tx := db.First(&session, "token = ?", cookie.Value); tx.Error != nil {
+// 		return nil, tx.Error
+// 	}
 
-	// logger.Info("SESS", fmt.Sprintf("Found session %s", session.Token))
+// 	// logger.Info("SESS", fmt.Sprintf("Found session %s", session.Token))
 
-	if session.isExpired() {
-		sm.Delete(db, session.Token)
+// 	if session.isExpired() {
+// 		sm.Delete(db, session.Token)
 
-		sm.activeSessions[session.AccountID] = nil
+// 		sm.activeSessions[session.AccountID] = nil
 
-		// logger.Info("SESS", fmt.Sprintf("Session for UserID %d expired, removed", session.AccountID))
-		return nil, errors.New("expired")
-	}
+// 		// logger.Info("SESS", fmt.Sprintf("Session for UserID %d expired, removed", session.AccountID))
+// 		return nil, errors.New("expired")
+// 	}
 
-	return session, nil
-}
+// 	return session, nil
+// }
 
-func (sm *sessionManagerImpl) Delete(db *gorm.DB, token Token) error {
-	var session *Session
-	if tx := db.Delete(&session, token); tx.Error != nil {
-		return tx.Error
-	}
-	log.Printf("Deleted token %s", session.Token)
-	return nil
-}
+// func (sm *sessionManagerImpl) Delete(db *gorm.DB, token Token) error {
+// 	var session *Session
+// 	if tx := db.Delete(&session, token); tx.Error != nil {
+// 		return tx.Error
+// 	}
+// 	log.Printf("Deleted token %s", session.Token)
+// 	return nil
+// }
