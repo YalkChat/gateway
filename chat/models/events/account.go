c9dbaba0ced88655cf4e4555305ed880d0025e70
AleRosmo <onair95@gmail.com>
2023-09-10 18:12:46 +0200

    Remove Account model

--- chat/models/events/account.go
+++
@@ -1,8 +0,0 @@
-package events
-
-type Account struct {
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Password string `json:"password"` // ! NOPE
-	Verified bool   `json:"verified"`
-}
