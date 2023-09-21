480e0d84db2ef05ef4c074c67b348b257b76ccfb
AleRosmo <onair95@gmail.com>
2023-09-22 00:35:51 +0200

    Remove pointless functions

--- handlers/new_connect.go
+++ handlers/new_connect.go
@@ -6,26 +6,10 @@ import (
 	"time"
 	"yalk/app"
 	"yalk/chat/client"
-	"yalk/chat/server"
-	"yalk/sessions"
 
 	"github.com/AleRosmo/cattp"
 	"nhooyr.io/websocket"
 )
-
-// TODO: Do these functions that only returns even make sense?
-
-func validateSession(r *http.Request, sessionsManager sessions.SessionManager) (*sessions.Session, error) {
-	return sessionsManager.Validate(r)
-}
-
-func upgradeToWebSocket(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
-	return websocket.Accept(w, r, nil)
-}
-
-func registerNewClient(client client.Client, srv server.Server) error {
-	return srv.RegisterClient(client)
-}
 
 // TODO: Placeholder, finish implementation
 func handleError(w http.ResponseWriter, err error) {
@@ -33,22 +17,19 @@ func handleError(w http.ResponseWriter, err error) {
 
 }
 
-func sendInitialPayload(srv server.Server, clientID uint) error {
-	return nil
-}
-
 // TODO: Handle Error must be finished
 var ConnectionHandler = cattp.HandlerFunc[app.HandlerContext](func(w http.ResponseWriter, r *http.Request, ctx app.HandlerContext) {
 	server := ctx.ChatServer()
 	sessionsManager := ctx.SessionsManager()
 
-	session, err := validateSession(r, sessionsManager)
+	session, err := sessionsManager.Validate(r)
 	if err != nil {
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
 		return
 	}
 
-	conn, err := upgradeToWebSocket(w, r)
+	// Upgrades to WebSocket
+	conn, err := websocket.Accept(w, r, nil)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
@@ -68,17 +49,13 @@ var ConnectionHandler = cattp.HandlerFunc[app.HandlerContext](func(w http.Respon
 		handleError(w, err)
 	}
 
-	err = registerNewClient(client, server)
+	err = server.RegisterClient(client)
+
 	if err != nil {
 		handleError(w, err)
 		return
 	}
-	// TODO: Consider if the initial payload should be done by the server instead
-	err = sendInitialPayload(server, client.ID())
-	if err != nil {
-		handleError(w, err)
-		return
-	}
+
 	fmt.Printf("registered new client: %d", client.ID())
 
 })
