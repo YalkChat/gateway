d1e888dcca5b276b069066c894d029ee0b92b5fe
AleRosmo <onair95@gmail.com>
2023-09-28 00:51:20 +0200

    Add properties for WebSocket settings

--- config/loader.go
+++ config/loader.go
@@ -9,16 +9,18 @@ import (
 )
 
 type Config struct {
-	DbHost        string
-	DbPort        string
-	DbUser        string
-	DbPassword    string
-	DbName        string
-	DbSslMode     string
-	HttpHost      string
-	HttpPort      string
-	HttpUrl       string
-	ClientTimeout time.Duration
+	DbHost                   string
+	DbPort                   string
+	DbUser                   string
+	DbPassword               string
+	DbName                   string
+	DbSslMode                string
+	HttpHost                 string
+	HttpPort                 string
+	HttpUrl                  string
+	ClientTimeout            time.Duration
+	WebSocketCompressionMode string
+	WebSocketReadLimit       string
 }
 
 func LoadConfig() (*Config, error) {
@@ -31,16 +33,18 @@ func LoadConfig() (*Config, error) {
 		clientTimeout = 5
 	}
 	config := Config{
-		DbHost:        os.Getenv("DB_HOST"),
-		DbPort:        os.Getenv("DB_PORT"),
-		DbUser:        os.Getenv("DB_USER"),
-		DbPassword:    os.Getenv("DB_PASSWORD"),
-		DbName:        os.Getenv("DB_NAME"),
-		DbSslMode:     os.Getenv("DB_SSLMODE"),
-		HttpHost:      os.Getenv("HTTP_HOST"),
-		HttpPort:      os.Getenv("HTTP_PORT_PLAIN"),
-		HttpUrl:       os.Getenv("HTTP_URL"),
-		ClientTimeout: time.Second * time.Duration(clientTimeout),
+		DbHost:                   os.Getenv("DB_HOST"),
+		DbPort:                   os.Getenv("DB_PORT"),
+		DbUser:                   os.Getenv("DB_USER"),
+		DbPassword:               os.Getenv("DB_PASSWORD"),
+		DbName:                   os.Getenv("DB_NAME"),
+		DbSslMode:                os.Getenv("DB_SSLMODE"),
+		HttpHost:                 os.Getenv("HTTP_HOST"),
+		HttpPort:                 os.Getenv("HTTP_PORT_PLAIN"),
+		HttpUrl:                  os.Getenv("HTTP_URL"),
+		ClientTimeout:            time.Second * time.Duration(clientTimeout),
+		WebSocketCompressionMode: os.Getenv("WS_COMPRESSION_MODE"),
+		WebSocketReadLimit:       os.Getenv("WS_READ_LIMIT"),
 	}
 
 	v := reflect.ValueOf(config)
