0359b1a22b3d16b8c119010546999ac77c1445ce
AleRosmo <onair95@gmail.com>
2023-10-03 02:18:37 +0200

    Add todo

--- chat/models/events/user_login.go
+++ chat/models/events/user_login.go
@@ -1,6 +1,7 @@
 package events
 
 // LoginPayload represents the payload for a user login event.
+// TODO: Can I remove the password from the User type then?
 type UserLogin struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
