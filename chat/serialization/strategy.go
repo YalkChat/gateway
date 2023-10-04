c5541f8e969a034fa1c8b9e0591e002f8684aa25
AleRosmo <onair95@gmail.com>
2023-10-05 01:16:09 +0200

    Add reminder

--- chat/serialization/strategy.go
+++ chat/serialization/strategy.go
@@ -1,5 +1,9 @@
 package serialization
 
+// The chat package provides only the interface, the implementation
+// is in the main package. The rest of the code should do the same
+// providing interfaces and leaving implementations outside
+
 type SerializationStrategy interface {
 	Serialize(any) ([]byte, error)
 	Deserialize([]byte, any) error
